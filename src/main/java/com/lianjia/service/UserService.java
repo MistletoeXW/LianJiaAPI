7bc2a7b6a49082fbbe83025f16f50d57bae10453
MistletoeXW <15178931042@qq.com>
2018-06-25 02:12:27 +0800

    Mybatis+热部署+swagger创建完成

--- src/main/java/com/lianjia/service/UserService.java
+++ src/main/java/com/lianjia/service/UserService.java
@@ -1,4 +1,14 @@
 package com.lianjia.service;
 
+import com.lianjia.dao.DataDetailMapper;
+import com.lianjia.dao.UserMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserMapper userMapper;
+    private DataDetailMapper dataDetailMapper;
+
+
 }
