d2ab57172c45340efa57928cb942fa237f2705d1
MistletoeXW <15178931042@qq.com>
2018-06-25 01:08:40 +0800

    完成项目搭建

---
+++ src/main/java/com/lianjia/LianjiaApiApplication.java
@@ -0,0 +1,12 @@
+package com.lianjia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianjiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LianjiaApiApplication.class, args);
+    }
+}
